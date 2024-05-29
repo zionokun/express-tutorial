1e850fb83e5f7a83584ff158ffcc39cdebf0ed50
Zion Okun <zeekun90@outlook.com>
2024-05-29 09:03:11 +0300

    update index js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Express.js!');
+    res.send('Hello from Express.js!!!!');
 });
 
 const PORT = process.env.PORT || 3000;
